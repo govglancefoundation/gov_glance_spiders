0c247fe80c49bdbda92ad7fc07cda320e800b44b
Santiago Saldivar <ssaldivar1@fordham.edu>
2024-02-15 18:30:17 -0500

    adding the spiders project

---
+++ src/legislative/congressional/congressional/pipelines.py
@@ -0,0 +1,154 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+import psycopg2
+import json
+import dateutil.parser
+from scrapy.utils.project import get_project_settings
+
+class CongressionalPipeline:
+    def process_item(self, item, spider):
+        bill_id = None
+
+        adapter = ItemAdapter(item)
+        field_names = adapter.field_names()
+  
+        if adapter['collection_name'] in ['Congressional Hearings', 'Public and Private Laws']:
+            field1_value = adapter['document_type']
+            cham = field1_value.split('HR')[0]
+            if cham == 'H':
+                full_title = adapter['title'].title()
+
+            else:
+                field2_value = adapter['congress']
+                field3_value = adapter['document_number']
+                title = adapter['title'].title()
+                full_title = f"{cham}. Hrg. {field2_value}-{field3_value} - {title}"
+            adapter['status_title'] = full_title
+        if adapter['collection_name'] in ['Congressional Bills', 'Congressional Bills Enrolled']:
+            field1_value = adapter['bill_type'].upper()
+            field2_value = adapter['bill_number']
+            field3_value = adapter['bill_version'].upper()
+            field4_value = adapter['title']
+            full_title = f"{field1_value}. {field2_value} ({field3_value}) - {field4_value}"
+            adapter['status_title'] = full_title
+        
+        for field_name in field_names:
+            if field_name == 'package_id':
+                bill_id = adapter[field_name]
+        for field_name in field_names:
+            value = adapter[field_name]
+            if value is not None:
+                if field_name == 'download':
+                    download_urls =adapter[field_name]
+                    for key, value in download_urls.items():
+                        if key in ['modsLink', 'premisLink']:
+                            url_string = value
+                            url_extention = url_string.split('/')[-1]
+                            download_url = url_string.replace("api","www").replace("/packages/", "/metadata/pkg/").replace(url_extention, '') +f'{url_extention}.xml'
+                            download_urls[key] = download_url
+
+                        if key == 'xmlLink':
+                            url_string = value
+                            url_extention = url_string.split('/')[-1]
+                            download_url = url_string.replace("api","www").replace("/packages/", "/content/pkg/")+f'/{bill_id}.xml'
+                            download_urls[key] = download_url
+                        if key == 'txtLink':
+                            url_string = value
+                            url_extention = url_string.split('/')[-1]
+                            download_url = url_string.replace("api","www").replace("/packages/", "/content/pkg/")+f'l/{bill_id}.{url_extention}'
+                            download_urls[key] = download_url
+                        if key == 'zipLink':
+                            url_string = value
+                            url_extention = url_string.split('/')[-1]
+                            download_url = url_string.replace("api","www").replace("/packages/", "/content/pkg/").replace(f"/{url_extention}","")+f'.{url_extention}'
+                            download_urls[key] = download_url
+                        if key == 'pdfLink':
+                            url_string = value
+                            url_extention = url_string.split('/')[-1]
+                            download_url = url_string.replace("api","www").replace("/packages/", "/content/pkg/")+f'/{bill_id}.{url_extention}'
+                            download_urls[key] = download_url
+                    adapter[field_name] = download_urls
+                if field_name in ['long_title', 'title']:
+                    value = adapter[field_name].title()
+                    adapter[field_name] = value
+                if field_name == 'created_at':
+                    value = adapter[field_name]
+                    adapter[field_name] = dateutil.parser.parse(value)
+                if field_name in ['download', 'related', 'other_identifiers', 'topics', 'committees', 'members', 'held_dates', 'serial_set', 'subjects', 'gov_info_references']:
+                    value = adapter[field_name]
+                    adapter[field_name] = json.dumps(value)
+
+        return item
+
+class ReadArticles:
+
+    def __init__(self):
+            
+            POSTGRES_USERNAME = get_project_settings().get('POSTGRES_USERNAME')
+            POSTGRES_PASS = get_project_settings().get('POSTGRES_PASSWORD')
+            POSTGRES_ADDRESS = get_project_settings().get('POSTGRES_ADDRESS')
+            POSTGRES_PORT = get_project_settings().get('POSTGRES_PORT')
+            POSTGRES_DBNAME = get_project_settings().get('POSTGRES_DBNAME')
+            self.schema = "united_states_of_america"
+            self.connection = psycopg2.connect(host=POSTGRES_ADDRESS, user=POSTGRES_USERNAME, password=POSTGRES_PASS, dbname=POSTGRES_DBNAME, port=POSTGRES_PORT)
+            self.cur = self.connection.cursor()
+    
+    def check_item(self,name, bill_id, modified_date): # the name variable is something that you write in the code block
+        
+        table = name.lower()
+        self.cur.execute(f"""SELECT package_id, created_at FROM {self.schema}.{table} WHERE package_id = '{bill_id}' AND created_at = '{modified_date}'""")
+        results = [i[0] for i in self.cur.fetchall()]
+        print(results)
+        if results:
+            return True
+        else:
+            return False
+
+
+class WriteCongressionalBills:
+
+    def __init__(self, POSTGRES_PASS, POSTGRES_USERNAME, POSTGRES_ADDRESS, POSTGRES_PORT, POSTGRES_DBNAME):
+            POSTGRES_USERNAME = POSTGRES_USERNAME
+            self.POSTGRES_PASS = POSTGRES_PASS
+            POSTGRES_ADDRESS = POSTGRES_ADDRESS
+            POSTGRES_PORT = POSTGRES_PORT
+            POSTGRES_DBNAME = POSTGRES_DBNAME
+            self.schema = "united_states_of_america"
+            self.connection = psycopg2.connect(host=POSTGRES_ADDRESS, user=POSTGRES_USERNAME, password=POSTGRES_PASS, dbname=POSTGRES_DBNAME, port=POSTGRES_PORT)
+            self.cur = self.connection.cursor()
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        settings = crawler.settings
+        POSTGRES_PASS = settings.get("POSTGRES_PASS")
+        POSTGRES_USERNAME = settings.get("POSTGRES_USERNAME")
+        POSTGRES_ADDRESS = settings.get("POSTGRES_ADDRESS")
+        POSTGRES_PORT = settings.get("POSTGRES_PORT")
+        POSTGRES_DBNAME = settings.get("POSTGRES_DBNAME")
+
+        return cls(POSTGRES_PASS, POSTGRES_USERNAME, POSTGRES_ADDRESS, POSTGRES_PORT, POSTGRES_DBNAME)
+
+    def process_item(self, item, spider): # Here we are going to get a dictionary or dataframe and publish new data
+        try:
+
+            table_name = item['collection_name'].lower().replace(' ', '_')
+            schema = self.schema
+            columns = ', '.join(item.keys())
+            values = ', '.join('%({})s'.format(key) for key in item.keys())
+            # values = ', '.join('%({})s'.format(key) if not 'references' else 'ARRAY[%({})s]::TEXT[]'.format(key) for key in item.keys())
+            query = f"INSERT INTO {schema}.{table_name} ({columns}) VALUES ({values})"
+            # print(query)
+            self.cur.execute(query, item)
+            self.connection.commit()
+        except psycopg2.Error as e:
+            print("Error: ", e)
+
+    def close_spider(self, spider):
+        self.cur.close()
+        self.connection.close()
---
+++ src/legislative/congressional/congressional/spiders/congressional_bills.py
@@ -0,0 +1,68 @@
+import scrapy
+import json
+import os
+from congressional.pipelines import ReadArticles
+from scrapy.utils.project import get_project_settings
+
+GOV_INFO_API_KEY = get_project_settings().get('GOV_INFO_API_KEY')
+
+class CongressionalBillsSpider(scrapy.Spider):
+    name = "congressional_bills"
+    allowed_domains = ["api.govinfo.gov"]
+    start_urls = [f"https://api.govinfo.gov/collections/BILLS/2023-01-01T20%3A18%3A10Z?pageSize=10&offsetMark=%2A&api_key={GOV_INFO_API_KEY}"]
+    
+
+    # need to get a list of packageId's from the api can compare them to our database. Yield a list and ten check which ones we do not have
+    def parse(self, response):
+        
+        data = json.loads(response.body)
+        for package in data['packages']:
+            scrapped = ReadArticles().check_item('congressional_bills', package['packageId'], package['lastModified'])
+            if scrapped == False:
+                item_id = package['packageId']
+                yield response.follow(f'https://api.govinfo.gov/packages/{item_id}/summary?api_key={GOV_INFO_API_KEY}', callback=self.parse_item)
+
+    def parse_item(self, response):
+        data = json.loads(response.body)
+        short_title = data.get('shortTitle')
+        if short_title != None:
+            title = short_title[0]['title'],
+            title = title[0]
+        else:
+            title = data.get('title')
+
+        yield {
+            'origin_chamber': data.get('originChamber'),
+            'url': data.get('detailsLink'),
+            'branch': data.get('branch'),
+            'congress': data.get('congress'),
+            'is_private': data.get('isPrivate'),
+            'long_title': data.get('title'),
+            'title': title,
+            'branch': data.get('branch'),
+            'is_appropriation': data.get('isAppropriation'),
+            'collection_name': data.get('collectionName'),
+            'download': data.get('download'),
+            'pages': data.get('pages'),
+            'related': data.get('related'),
+            'related_url': data.get('relatedLink'),
+            'members': data.get('members'),
+            'su_doc_class_number' : data.get('suDocClassNumber'),
+            'date_issued': data.get('dateIssued'),
+            'current_chamber': data.get('currentChamber'),
+            'bill_version': data.get('billVersion'),
+            'bill_type': data.get('billType'),
+            'package_id': data.get('packageId'),
+            'committees': data.get('committees'),
+            'committee': data.get('committeeName'),
+            'collection_code': data.get('collectionCode'),
+            'government_author1': data.get('governmentAuthor1'),
+            'government_author2': data.get('governmentAuthor2'),
+            'publisher': data.get('publisher'),
+            'doc_class': data.get('docClass'),
+            'created_at': data.get('lastModified'),
+            'category': data.get('category'),
+            'bill_number': data.get('billNumber'),
+            'other_identifiers': data.get('otherIdentifier')
+        }
+        
